5b85dd16fd4d29a03064e83b116c7b2ea542f854
Robin Duckett <robin.duckett@gmail.com>
2020-04-30 23:37:12 +0100

    Fixing BLK by adding readProgress back in

--- src/index-blk.js
+++ src/index-blk.js
@@ -92,3 +92,9 @@ stream.on("image", (info) => {
     }
   );
 });
+
+function readProgress(readBytes) {
+  console.log("read", (readBytes / 1024 / 1024).toFixed(2), "mb");
+
+  setTimeout(() => stream.once("progress", readProgress), 1000);
+}
